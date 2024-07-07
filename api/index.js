ebec88a7355df344520a29c5942622b64d5a8c7b
Yotam Adler <yotam.adler@gmail.com>
2024-07-07 15:40:24 +0300

    Login page and style

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ var bcrypt = require('bcrypt');
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './public')));
 app.get('/', function (req, res) {
     res.render("login");
 });
